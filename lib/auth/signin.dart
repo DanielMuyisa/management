a72c658319d03ab952112a2479dec763d1ec858e
Daniel Muyisa <danielmuyisa37@gmail.com>
2022-10-10 11:23:07 +0100

    upgrade signin

---
+++ lib/auth/signin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Signin extends StatefulWidget {
+  const Signin({Key? key}) : super(key: key);
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
